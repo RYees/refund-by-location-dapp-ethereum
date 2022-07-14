4bc108377d68e5761266cfe64fbdba46875dfb2b
netes <toygame8888@gmail.com>
2022-07-14 18:31:37 +0300

    contract

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/mTqKDgkJW6to-0hQpZ9irfemd9WvJTvv
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.4.18',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/mTqKDgkJW6to-0hQpZ9irfemd9WvJTvv',
+      accounts: ['03c998ad8bd0d67e2655a9982c2f1272626f3bc17edf8e57a218c264a04e3b39'],
+    },
+  },
 };
